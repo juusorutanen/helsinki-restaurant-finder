4838323a33dea8786fac24b17fc1388a52d750cb
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-08-09 09:56:51 +0300

    change card styles

--- app/components/places/PlaceCard.tsx
+++ app/components/places/PlaceCard.tsx
@@ -8,8 +8,6 @@ import HeartButton from "../HeartButton";
 import styles from "../../../styles/components/PlaceCard.module.scss"
 
 
-
-
 interface PlaceCardProps {
     data: Place;
     currentUser?: SafeUser | null;
@@ -21,30 +19,33 @@ const PlaceCard: React.FC<PlaceCardProps> =({
     const router = useRouter();
 
     return (
-        <div className={styles.gridChild} onClick={() => router.push(`/places/${data.id}`)}>
-            <div className={styles.flexColumn}>
-                <div className={styles.relative}>
-                    <Image
-                    fill
-                    alt="Restaurant"
-                    src={data.picture_url || ''}
-                    style={{ width: '100%', height: '100%', objectFit:"cover" }}
-                    />
-                   
+                <div className={styles.card}>
+                    <div className={styles.cardImageContainer} onClick={() => router.push(`/places/${data.id}`)}>
+                        <Image
+                        width={1000}
+                        height={1000}
+                        className='nextimg'
+                        alt="Restaurant"
+                        src={data.picture_url || ''}
+                        
+                        />
+                    </div>
                     <div className={styles.heartButton}>
                         <HeartButton
                         placeId={data.id}
                         currentUser={currentUser}/>
                     </div>
-                    <div className={styles.name}>
-                        <p>{data.name}</p>
+                    <div className={styles.infoContainer}>
+                        <div className={styles.info}>
+                            <p className={styles.name}>{data.name}</p>
+                            <p className={styles.address}>{data.street_address}</p>
+                        </div>
+                        <div className={styles.url}>
+                            <a href={data.www || ''} target="_blank">Website</a>
+                        </div>
                     </div>
-                    <div className={styles.url}>
-                        <a href={data.www || ''}>Website</a>
-                    </div>
-                </div>
+    
             </div>
-        </div>
       );
 }
  
