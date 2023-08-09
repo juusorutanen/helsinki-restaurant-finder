495cb321ddee3b2de1c76970f4277908b02c67bb
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-08-09 16:31:27 +0300

    make navbar disappear when scrolling

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Categories from './Categories';
 import Image from '../navbar/Logo';
 import { SafeUser } from '@/app/types';
 import SearchBar from '../SearchBar';
+import { useState,useEffect } from 'react';
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
@@ -13,10 +14,28 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({
     currentUser
-}) => {
+
     
+}) => {
+
+    const [show, setShow] = useState(true)
+    const controlNavbar = () => {
+        if (window.scrollY > 250) {
+            setShow(false)
+        } else {
+            setShow(true)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlNavbar)
+        return () => {
+            window.removeEventListener('scroll', controlNavbar)
+        }
+    }, [])
+
     return (
-        <div className ={styles.navbar}>
+        <div className={`${styles.navbar} ${show ? styles.show : ''}`}>
             <div className={styles.navbarBorder}>
             <Container>
                 <div className={styles.navbarContainer}>
