e184b3813692ca22d49732ff2283bcfaeecdff22
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-09-04 23:01:19 +0300

    fix pagination errors

--- app/components/PaginationBar.tsx
+++ app/components/PaginationBar.tsx
@@ -1,6 +1,7 @@
-import Link from "next/link";
-import styles from "../../styles/components/PaginationBar.module.scss";
-import Container from "./Container";
+'use client'
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import styles from '../../styles/components/PaginationBar.module.scss';
 
 interface PaginationBarProps {
   currentPage: number;
@@ -11,18 +12,29 @@ export default function PaginationBar({
   currentPage,
   totalPages,
 }: PaginationBarProps) {
+  const searchParams = useSearchParams();
+
+  if (!searchParams) {
+    return null;
+  }
+  const pageParam = searchParams.get('page');
+  const currentPageNumber = pageParam ? parseInt(pageParam, 5) : 1;
+
   const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 5));
   const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
 
   const numberedPageItems: JSX.Element[] = [];
 
   for (let page = minPage; page <= maxPage; page++) {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set('page', page.toString());
+
     numberedPageItems.push(
       <Link
-        href={"?page=" + page}
+        href={{ pathname: '/', search: `?${newSearchParams.toString()}` }}
         key={page}
-        className={`${styles["join-item"]} ${styles.btn} ${
-          currentPage === page ? styles["btn-active"] : ""
+        className={`${styles['join-item']} ${styles.btn} ${
+          currentPageNumber === page ? styles['btn-active'] : ''
         }`}
       >
         {page}
@@ -31,35 +43,58 @@ export default function PaginationBar({
   }
 
   return (
-    <>
     <div className={styles.paginationBar}>
-  <div className={`${styles.join} ${styles["block"]} ${styles["sm:hidden"]}`}>
-    {currentPage > 1 && (
-      <div className={`${styles["join-item"]} ${styles.btn}`}>
-        <Link href={"?page=" + (currentPage - 1)}>«</Link>
+      <div className={styles.join}>
+        {currentPageNumber > 1 && (
+          <div className={styles.joinItem}>
+            <Link
+              href={{
+                pathname: '/',
+                search: `?${new URLSearchParams({
+                  ...Object.fromEntries(searchParams),
+                  page: (currentPageNumber - 1).toString(),
+                }).toString()}`,
+              }}
+            >
+              «
+            </Link>
+          </div>
+        )}
       </div>
-    )}
-  </div>
 
-  <div className={styles.join}>
-    {numberedPageItems.map((item, index) => (
-      <div
-        className={`${styles["join-item"]} ${currentPage === index + minPage ? styles["btn-active"] : ""}`}
-        key={index}
-      >
-        <Link href={"?page=" + (index + minPage)}>{index + minPage}</Link>
+      <div className={styles.join}>
+        {numberedPageItems.map((item, index) => (
+          <div
+            className={`${styles['join-item']} ${
+              currentPageNumber === index + minPage ? styles['btn-active'] : ''
+            }`}
+            key={index}
+          >
+            {item}
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
 
-  <div className={`${styles.join} ${styles["block"]} ${styles["sm:hidden"]}`}>
-    {currentPage < totalPages && (
-      <div className={`${styles["join-item"]} ${styles.btn}`}>
-        <Link href={"?page=" + (currentPage + 1)}>»</Link>
+      <div className={styles.join}>
+        {currentPageNumber < totalPages && (
+          <div className={styles.joinItem}>
+            <Link
+              href={{
+                pathname: '/',
+                search: `?${new URLSearchParams({
+                  ...Object.fromEntries(searchParams),
+                  page: (currentPageNumber + 1).toString(),
+                }).toString()}`,
+              }}
+            >
+              »
+            </Link>
+          </div>
+        )}
       </div>
-    )}
-  </div>
-</div>
-    </>
+    </div>
   );
 }
+
+
+
